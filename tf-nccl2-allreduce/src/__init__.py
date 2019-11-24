17e6f9d2af3190dd3988dbc77f13ae81b91dff94
ghostplant <12099308+ghostplant@users.noreply.github.com>
2019-11-24 17:49:02 +0000

    eliminate partial gradients

--- tf-nccl2-allreduce/src/__init__.py
+++ tf-nccl2-allreduce/src/__init__.py
@@ -7,6 +7,7 @@ import os
 from tensorflow.contrib.util import loader
 from tensorflow.python.platform import resource_loader
 from mpi4py import MPI
+import numpy as np
 
 __ops_name__ = __loader__.name.split('.')[-1]
 
@@ -27,9 +28,19 @@ def broadcast_global_variables(sourceRank=0):
 
 def allreduce(grad_gv):
   # MPI.COMM_WORLD.Barrier()
+  non_super_grad = []
+  inter_param, local_param = 0, 0
+  for g, v in grad_gv:
+    if 'super_dense_scope' not in v.name:
+      non_super_grad.append((g, v))
+      inter_param += int(np.product(v.shape))
+    else:
+      local_param += int(np.product(v.shape))
+  print('Total parameter count = %d / inter, %d / local.' % (inter_param, local_param))
   _, size, _ = get_node_config()
   if size == 1:
     return grad_gv
+  grad_gv = non_super_grad
   grad_g = [g for g, _ in grad_gv]
   grad_v = [v for _, v in grad_gv]
   grad_g = library.nccl2_allreduce(grad_g)
@@ -38,31 +49,30 @@ def allreduce(grad_gv):
 
 @tf.custom_gradient
 def super_dense_preprocess(x):
+  rank, size, _ = get_node_config()
   def grad(dy):
+    if size == 1:
+      return dy
     return library.nccl2_allreduce([dy])
   return x, grad
 
-# e.g. y = nccl2_allreduce.super_dense(y, out_dim=2)
-def super_dense(data, out_dim, activation=None, kernel_initializer=None, name=None):
+# e.g. y = nccl2_allreduce.super_dense(y, units=2)
+def super_dense(data, units, activation=None, kernel_initializer=None, name=None):
   rank, size, _ = get_node_config()
-  assert(out_dim % size == 0)
+  assert(units % size == 0)
   x = super_dense_preprocess(data)
 
-  if name is None:
-    import uuid
-    name = str(uuid.uuid1())
-  partial_w = tf.get_variable("partial_w-" + name, shape=[int(data.shape[1]), out_dim // size], dtype=data.dtype, initializer=kernel_initializer)
-  partial_b = tf.get_variable("partial_b-" + name, shape=[1, out_dim // size], dtype=data.dtype, initializer=tf.compat.v1.zeros_initializer())
+  with tf.variable_scope('super_dense_scope'):
+    y = tf.compat.v1.layers.dense(x, units=units // size, activation=activation, kernel_initializer=kernel_initializer, name=name)
 
-  y = tf.matmul(x, partial_w)
-  y = tf.add(y, partial_b)
-  if activation is not None:
-    y = activation(y)
-  z = library.super_dense_postprocess(y)
-
+  batch_size = int(data.shape[0])
   if size > 1:
-    z = tf.transpose(z, [1, 0, 2])
-  out = tf.reshape(z, [int(data.shape[0]), out_dim])
+    z = library.super_dense_postprocess(y)
+    if batch_size > 1:
+      z = tf.transpose(z, [1, 0, 2])
+  else:
+    z = y
+  out = tf.reshape(z, [batch_size, units])
   return out
 
 @tf.RegisterGradient("SuperDensePostprocess")
